ee64375ccfd1f8b2065b61b985d70935b5abbdf3
suvo <19202103281@cse.bubt.edu.bd>
2021-01-05 18:59:11 +0600

    massage su

---
+++ Untitled2.cpp
@@ -0,0 +1,16 @@
+//class+function
+//student attribute
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+   
+
+    	cout<<"stu1.name 2"<<endl;
+    	cout<<"stu1.name 2"<<endl;
+	cout<<"stu1.name 2"<<endl;
+	cout<<"stu1.name 2"<<endl;
+
+}
